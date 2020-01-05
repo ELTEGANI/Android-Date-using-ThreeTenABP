cbe2e95ced9c09ce0c9584c7a30d95dc9a4952a1
Tiganimohammad <tiganimoh@gmail.com>
2020-01-05 13:26:41 +0200

    displayed dates using ThreeTenABP

---
+++ app/src/main/java/com/learning/androiddateandtime/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.learning.androiddateandtime
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jakewharton.threetenabp.AndroidThreeTen
+import kotlinx.android.synthetic.main.activity_main.*
+import org.threeten.bp.LocalDate
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        AndroidThreeTen.init(this)
+
+
+        val date = LocalDate.now()
+
+
+        today_textView.text = date.toString()
+
+
+        weekstart_textView.text = date.toString()
+        weekend_textView.text = date.plusDays(7).toString()
+
+
+        monthstart_textView.text = date.toString()
+        monthend_textView.text = date.plusMonths(1).toString()
+
+
+    }
+}
